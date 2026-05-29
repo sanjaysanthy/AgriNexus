1f19eaade14b52ca9e3f747898cec7328b51ade5
Sanjay <sanjaysanthy2607@gmail.com>
2026-05-29 18:35:00 +0530

    Productionn

--- web/server/server.js
+++ web/server/server.js
@@ -24,8 +24,6 @@ app.use(
   })
 );
 
-// Handle preflight globally
-app.options("*", cors());
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
